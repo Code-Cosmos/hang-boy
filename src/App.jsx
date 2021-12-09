fe549a26a5ca3632f8c4f07dd79e7471e356adf3
Tom Brown <tombrown@Toms-MacBook-Air.local>
2021-12-09 15:51:42 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import HangBoy from "./HangBoy";
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <HangBoy />
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/HangBoy.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class HangBoy extends Component {
+  render() {
+    return (
+      <section className="Hang-Boy">
+        <h1>HI I AM HANGBOY</h1>
+      </section>
+    );
+  }
+}
+export default HangBoy;
